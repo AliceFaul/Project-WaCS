3ff072f4d2fd3f9681176b3aa16cb9e842b1e813
33Bruh <maihieu6714@gmail.com>
2026-02-24 22:58:16 +0700

    Updated game state

---
+++ WaCS/Assets/_Project/Core/StateMachine/ShopCloseState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ShopCloseState : IGameState
+{
+    public void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
